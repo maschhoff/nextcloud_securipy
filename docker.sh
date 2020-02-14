848ec3a340f49616b43216604598756972cc325f
Mathias Aschhoff <web@little-devil.de>
2020-02-14 18:20:42 +0100

    Add files via upload

---
+++ docker.sh
@@ -0,0 +1,1 @@
+docker run -it --rm -v /mnt/user/appdata/ncsecuripy:/ncsecuripy frolvlad/alpine-python3 sh /ncsecuripy/run.sh
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /ncsecuripy/
+python3 -m pip install -r requirements.txt
+python3 check.py
